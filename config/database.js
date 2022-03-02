1ccbb790cb58b386b4b6f90714971d608e6abb2a
Faith Johnson <faithpjohnson@gmail.com>
2022-03-02 08:59:55 -0700

    commit

--- config/database.js
+++ config/database.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb://localhost:27017/project-4",
-);
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 const db = mongoose.connection;
 
-db.on('connected', function() {
+db.on("connected", function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
 
-db.on('error', function(err){
-  console.log(`Mongodb error: ${err}`)
-})
+db.on("error", function (err) {
+  console.log(`Mongodb error: ${err}`);
+});
